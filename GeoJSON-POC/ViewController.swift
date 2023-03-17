da255279d2dfba7c46993aec828c7149138237e6
Sam Haycraft <samuel.g.haycraft@xcelenergy.com>
2023-03-17 11:20:45 -0600

    Initial Commit

---
+++ GeoJSON-POC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeoJSON-POC
+//
+//  Created by Samuel Haycraft on 3/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
